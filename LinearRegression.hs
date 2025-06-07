ff871400eb4cfc4a66db5a7e4f94929d0aa07982
Artem <135530171+artem-hontarenko@users.noreply.github.com>
2025-06-07 13:59:31 +0200

    Update LinearRegression.hs

--- LinearRegression.hs
+++ LinearRegression.hs
@@ -3,33 +3,29 @@ module LinearRegression(newWeights) where
 import Data.List 
 import RandomSampling
 
--- Calculate new values for t0 and t1
--- newWeights :: Coefficients -> LerningRate -> Data -> Coefficients
+-- Calculate new values for weights
 newWeights :: [Double] -> Double -> [[Double]] -> IO [Double]
 newWeights weights alpha dataset = do
     batch <- sampleDataUnique (length dataset `div` 2) dataset 
     let deltas = map (calculateDelta weights) batch
-        adjustedDeltas = adjustDeltas deltas batch  
-        newW0 = w0 - alpha * avg deltas 
-        newW1 = w1 - alpha * avg adjustedDeltas
-    return [newW0, newW1]
-    where 
-        w0 = weights !! 0
-        w1 = weights !! 1
-    
--- Calculates the difference between h(x) and y
+        numFeatures = length weights - 1
+        adjustedDeltas = [zipWith(*) deltas (ithFeatureColumn i batch) | i <- [0..numFeatures - 1]]
+        newW0 = head weights - alpha * avg deltas
+        newW1_Wn = [weights !! (i+1) - alpha * avg(adjustedDeltas !! i) | i <- [0..numFeatures - 1]]
+    return (newW0 : newW1_Wn)
+
+-- Calculate difference between prediction and actual target
 calculateDelta :: [Double] -> [Double] -> Double
-calculateDelta weights row = w0 + w1 * x - y
-  where
-    w0 = weights !! 0
-    w1 = weights !! 1
-    x = row !! 0
-    y = row !! 1
-
-adjustDeltas :: [Double] -> [[Double]] -> [Double]
-adjustDeltas deltas dataset = 
-    zipWith (*) deltas features
+calculateDelta weights row = predict_y - y
     where 
-        features = map head dataset
+        y = last row
+        features = init row
+        w0 = head weights
+        predict_y = w0 + sum (zipWith (*) (tail weights) features)
 
+-- Get the column consists of i-th values from every entry in the batch 
+ithFeatureColumn :: Int -> [[Double]] -> [Double]
+ithFeatureColumn columnID = map (!! columnID)   
+
+-- Compute average value for the list
 avg xs = realToFrac (sum xs) / genericLength xs
